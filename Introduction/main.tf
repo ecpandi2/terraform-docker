10944c1b017ff67bed8e6e07238952a3c6b52cfd
Pandiyan <pandiec@gmail.com>
2022-07-20 22:40:35 +0530

    Terraform docker provider

---
+++ Introduction/main.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+      version = "2.19.0"
+    }
+  }
+}
+
+provider "docker" {
+  # Configuration options
+}
+
