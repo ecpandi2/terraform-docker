1869921a1e798b8c2758551c528617ecebcb4967
Pandiyan <pandiec@gmail.com>
2022-07-21 13:41:56 +0530

    For loop

--- Introduction/main.tf
+++ Introduction/main.tf
@@ -1,12 +1,17 @@
 terraform {
   required_providers {
     docker = {
-      source = "kreuzwerkers/docker"
+      source = "kreuzwerker/docker"
+      version = "2.19.0"
     }
   }
 }
 
-provider "docker" {}
+provider "docker" {
+  # Configuration options
+}
+
+
 
 resource "docker_image" "nodered_image" {
   name = "nodered/node-red:latest"
