94d3d999d2551a3bf192c8496b3ad5472a078a31
Pandiyan <pandiec@gmail.com>
2022-07-21 12:19:40 +0530

    Multiple-Resources-count

--- Introduction/main.tf
+++ Introduction/main.tf
@@ -2,45 +2,51 @@ terraform {
   required_providers {
     docker = {
       source = "kreuzwerker/docker"
-      version = "2.19.0"
     }
   }
 }
 
-provider "docker" {
-  # Configuration options
-}
-# download nodered image
+provider "docker" {}
 
 resource "docker_image" "nodered_image" {
   name = "nodered/node-red:latest"
 }
 
 resource "random_string" "random" {
-  length           = 4
-  count            = 2
-  special          = false
-  upper            = false
+  count   = 2
+  length  = 4
+  special = false
+  upper   = false
 }
 
-# start the container
 
 resource "docker_container" "nodered_container" {
+  count = 2
   name  = join("-", ["nodered", random_string.random[count.index].result])
   image = docker_image.nodered_image.latest
   ports {
     internal = 1880
-    #external = 1880
+    # external = 1880
   }
 }
 
-#Output the IP Address of the Container
-output "ip-Address" {
-  value       = join(":",[docker_container.nodered_container.ip_address, docker_container.nodered_container[*].ports[0].external])
-  description = "The IP address of the container"
+
+output "ip-address" {
+  value       = join(":", [docker_container.nodered_container[0].ip_address, docker_container.nodered_container[0].ports[0].external])
+  description = "The IP address and external port of the container"
 }
 
 output "container-name" {
-  value       = docker_container.nodered_container.name
+  value       = docker_container.nodered_container[0].name
+  description = "The name of the container"
+}
+
+output "ip-address2" {
+  value       = join(":", [docker_container.nodered_container[1].ip_address, docker_container.nodered_container[1].ports[0].external])
+  description = "The IP address and external port of the container"
+}
+
+output "container-name2" {
+  value       = docker_container.nodered_container[1].name
   description = "The name of the container"
 }
