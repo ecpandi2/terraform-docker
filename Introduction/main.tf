2d040a944bb7c92fc1e7e6d89c4873f963e59241
Pandiyan <pandiec@gmail.com>
2022-07-21 09:31:16 +0530

    Referencing-Other-Resources

--- Introduction/main.tf
+++ Introduction/main.tf
@@ -10,7 +10,19 @@ terraform {
 provider "docker" {
   # Configuration options
 }
+# download nodered image
 
 resource "docker_image" "nodered_image" {
   name = "nodered/node-red:latest"
+}
+
+# start the container
+
+resource "docker_container" "nodered_container" {
+  name  = "nodered"
+  image = docker_image.nodered_image.latest
+  ports {
+    internal = 1880
+    external = 1880
+  }
 }
