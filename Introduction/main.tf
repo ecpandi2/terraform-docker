313b0df6a144f1c7d151a1635a096e0b6f4473a8
Pandiyan <pandiec@gmail.com>
2022-07-21 14:02:27 +0530

    Adding-Variables/

--- Introduction/main.tf
+++ Introduction/main.tf
@@ -2,43 +2,53 @@ terraform {
   required_providers {
     docker = {
       source = "kreuzwerker/docker"
-      version = "2.19.0"
     }
   }
 }
 
-provider "docker" {
-  # Configuration options
+
+provider "docker" {}
+
+# variables
+
+
+variable "int_port" {
+  type    = number
+  default = 1880
 }
 
+variable "ext_port" {
+  type    = number
+  default = 1880
+}
 
+variable "container_count" {
+  type    = number
+  default = 2
+}
+
+# download nodered image
 
 resource "docker_image" "nodered_image" {
   name = "nodered/node-red:latest"
 }
 
-resource "random_string" "random" {
-  count   = 2
-  length  = 4
-  special = false
-}
+# start the container
 
 resource "docker_container" "nodered_container" {
-  count = 2
-  name  = join("-", ["nodered", random_string.random[count.index].result])
+  name  = "nodered"
   image = docker_image.nodered_image.latest
   ports {
-    internal = 1880
-    # external = 1880
+    internal = var.int_port
+    external = var.ext_port
   }
 }
 
-output "ip_address" {
-  value       = [for i in docker_container.nodered_container[*] : join(":", [i.ip_address], i.ports[*]["external"])]
-  description = "The IP address of the container"
+#Output the IP Address of the Container
+output "ip-address" {
+  value = docker_container.nodered_container.ip_address
 }
 
-output "container_name" {
-  value       = docker_container.nodered_container[*].name
-  description = "The name of the container"
+output "container-name" {
+  value = docker_container.nodered_container.name
 }
