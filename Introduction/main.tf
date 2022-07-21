a3bb29da32033c745f28ef8920cbe54ab93d8e47
Pandiyan <pandiec@gmail.com>
2022-07-21 14:05:26 +0530

    Variable-Validation

--- Introduction/main.tf
+++ Introduction/main.tf
@@ -11,21 +11,32 @@ provider "docker" {}
 
 # variables
 
+variable "container_name" {
+  type        = string
+  description = "Name of Container"
+  default     = "nodered"
+}
 
 variable "int_port" {
   type    = number
   default = 1880
+
+  validation {
+    condition     = var.int_port == 1880
+    error_message = "The internal port for node-red can only be 1880."
+  }
 }
 
 variable "ext_port" {
   type    = number
   default = 1880
+
+  validation {
+    condition     = var.ext_port <= 65535 && var.ext_port > 0
+    error_message = "The external port must be in the valid port range 0 - 65535."
+  }
 }
 
-variable "container_count" {
-  type    = number
-  default = 2
-}
 
 # download nodered image
 
@@ -36,7 +47,7 @@ resource "docker_image" "nodered_image" {
 # start the container
 
 resource "docker_container" "nodered_container" {
-  name  = "nodered"
+  name  = var.container_name
   image = docker_image.nodered_image.latest
   ports {
     internal = var.int_port
